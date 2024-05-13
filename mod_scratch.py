93d91521a35dddb20a1c64607596caca520d828f
Erdal Genc <ERDG@equinor.com>
2024-05-13 12:34:58 +0100

    new dataset added, code refactored

--- mod_scratch.py
+++ mod_scratch.py
@@ -6,14 +6,24 @@ import matplotlib.pyplot as plt
 import random
 import math
 from sklearn import datasets
-
+import os
 from methods import GradientDescent, ProposedDescent, StochasticDescent
 
-iris = datasets.load_iris()
 
-X = iris.data
-Y = iris.target
+dataset = "diabetes"
+SAVE_DIR = f"C:/Users/ERDG/Documents/repos/results/gradient-new/{dataset}/"
+os.makedirs(SAVE_DIR, exist_ok=True)
+data = datasets.load_diabetes()
 
+X = data.data
+Y = data.target
+
+# df = pd.read_csv("C:/Users/ERDG/Documents/repos/gradient-new/Advertising.csv")
+
+
+# X = df[['TV', 'Radio', 'Newspaper']].to_numpy()
+# Y = df['Sales'].to_numpy()
+# print(X)
 '''
 # Uncomment this code to use the loaded csv instead of the iris dataset
 # Slice features and target from the advertising dataset
@@ -30,6 +40,8 @@ ITER_LIST = [1000, 10_000, 50_000]
 
 JUMP_SIZES = [2, 3, 4, 5, 8, 10]
 
+
+
 # Random initialization for bias and theta
 def initialize(dim):
     b = random.random()
@@ -40,19 +52,21 @@ def initialize(dim):
 for it in ITER_LIST:
     for lr in LR_LIST:
         time_results = {}
-        for jump_size in JUMP_SIZES:
-            # Define the models to run
-            models = {"stochastic_gradient_descent": StochasticDescent,
-                    "gradient": GradientDescent,
-                    "proposed": ProposedDescent}
 
-            # Define the model parameters
-            bias, weig = initialize(X.shape[1])
-            params = {"iterations": it, "lr": lr}
+        bias, weig = initialize(X.shape[1])
+
+        params = {"iterations": it, "lr": lr}
+
+        models = {"stochastic_gradient_descent": StochasticDescent,
+        "gradient": GradientDescent,
+        "proposed": ProposedDescent}
+        results_dict = {}
+        times_dict = {}
+        for jump_size in JUMP_SIZES:
 
             # Run the models and store the results and execution times in a dictionary
-            results = {}
-            times_dict = {}
+            result_tmp = {}
+            times_tmp = {}
             for model_name, model_class in models.items():
                 if model_name == "proposed":
                     params["jump_size"] = jump_size
@@ -61,23 +75,29 @@ for it in ITER_LIST:
                 else:
                     times = f"{model_name}_time_elapsed"
                     result_name = f"{model_name}_costs"
+                    params.pop("jump_size", None)
+
                 start = time.time()
                 model = model_class(weig, bias)
                 costs, weights = model.fit(X, Y, **params)
                 end = time.time()
                 elapsed_time = end - start
-                times_dict[times] = elapsed_time
-                results[result_name] = costs
+                times_tmp[times] = elapsed_time
+                result_tmp[result_name] = costs
 
+                results_dict.update(result_tmp)
+                times_dict.update(times_tmp)
+
+        
         series_dict = {}
-        for k, v in results.items():
+        for k, v in results_dict.items():
             series_dict[k] = pd.Series(v)
 
         df = pd.DataFrame(series_dict)
 
         df.to_csv(
-            f"C:/Users/ERDG/Documents/repos/results/iris_lr{lr}_iter{it}.csv", index=False)
+            f"{SAVE_DIR}/{dataset}_lr{lr}_iter{it}.csv", index=False)
         times_df = pd.DataFrame.from_dict(
-            times_dict, orient="index", columns=["time_elapsed"])
+            times_dict, orient="index")
         times_df.to_csv(
-            f"C:/Users/ERDG/Documents/repos/results/TIMES_iris_lr{lr}_iter{it}.csv", index_label="model_name")
+            f"{SAVE_DIR}/TIMES_{dataset}_lr{lr}_iter{it}.csv", index_label="model_name")
